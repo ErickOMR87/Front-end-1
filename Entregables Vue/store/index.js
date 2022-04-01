798b1430f560b5c6bc6a097f8af5556f8dbf873d
Erick Martínez <99059630+ErickOMR87@users.noreply.github.com>
2022-04-01 09:46:45 -0600

    Add files via upload

---
+++ Entregables Vue/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    paqueteMario: 225,
+    paqueteLuigui: 225,
+    paqueteYoshi: 225,
+    adorno: 50,
+    chico:100,
+    mediano:150,
+    grande:200,
+
+    
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
